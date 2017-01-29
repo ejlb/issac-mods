296d33add6f3cf8a72852a7d0241e6e502fb8a70
Eddie Bell <ejlbell@gmail.com>
2017-01-29 00:49:43 +0000

    rename

--- 01-overpowered/main.lua
+++ 01-overpowered/main.lua
@@ -1,10 +1,10 @@
-local Test = RegisterMod("Test", 1)
+local Overpowered = RegisterMod("Overpowered", 1)
 
-function Test:fast(player, cacheFlag)
+function Overpowered:fast(player, cacheFlag)
     if cacheFlag == CacheFlag.CACHE_DAMAGE then
         player.Damage = player.Damage + 50
         player.MaxFireDelay = 5
     end
 end
 
-Test:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Test.fast)
+Overpowered:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Overpowered.fast)
