09227598ba747e9419c048f544a39c356dd9660f
Eddie Bell <ejlbell@gmail.com>
2017-01-29 11:39:39 +0000

    tear effects

--- 01-overpowered/main.lua
+++ 01-overpowered/main.lua
@@ -1,10 +1,18 @@
 local Overpowered = RegisterMod("Overpowered", 1)
 
-function Overpowered:fast(player, cacheFlag)
+function Overpowered:onCache(player, cacheFlag)
     if cacheFlag == CacheFlag.CACHE_DAMAGE then
-        player.Damage = player.Damage + 50
+        player.Damage = 50
         player.MaxFireDelay = 5
+    end
+
+    if cacheFlag == CacheFlag.CACHE_TEARFLAG then
+        player.TearFlags = player.TearFlags | 1 
+        player.TearFlags = player.TearFlags | 1 << 1
+        player.TearFlags = player.TearFlags | 1 << 2
+        player.TearFlags = player.TearFlags | 1 << 18
+        player.TearFlags = player.TearFlags | 1 << 53
     end
 end
 
-Overpowered:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Overpowered.fast)
+Overpowered:AddCallback(ModCallbacks.MC_EVALUATE_CACHE, Overpowered.onCache)
